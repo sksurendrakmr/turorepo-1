b807e9fb13b5b105b94f236eabac57c4c6c2e781
sksurendrakmr <sksurendrakmr@gmail.com>
2022-10-06 23:29:18 +0530

    working and correct setup

--- apps/atoms/CHANGELOG.md
+++ apps/atoms/CHANGELOG.md
@@ -1,13 +1,43 @@
 # @surikmr1/atoms
 
-## 3.0.0
+## 6.3.1
+
+### Patch Changes
+
+- ll
+
+## 6.3.0
+
+### Minor Changes
+
+- l
+
+## 6.2.1
+
+### Patch Changes
+
+- oo
+
+## 6.2.0
+
+### Minor Changes
+
+- oo
+
+## 6.1.0
+
+### Minor Changes
+
+- check
+
+## 6.0.0
 
 ### Major Changes
 
-- second
+- okk
 
-## 2.0.0
+## 5.1.0
 
-### Major Changes
+### Minor Changes
 
-- first publish
+- ok
--- apps/atoms/webpack.config.js
+++ apps/atoms/webpack.config.js
@@ -1,21 +1,22 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const package = require('./package.json');
 module.exports = {
-    entry:path.resolve(__dirname,'./src/index.ts'),
+    entry:path.resolve(__dirname,package.module),
     output:{
-        path:path.resolve(__dirname,'build'),
-        filename:'bundle.js',
+        path:path.resolve(__dirname),
+        filename:package.main,
         library:'@surikmr1/atoms',
         libraryTarget:"umd",
         globalObject:'this',
-        clean:true
+        // clean:true
     },
-    devtool:'source-map',
+    // devtool:'source-map',
     module:{
         rules:[
             {
                 test:/\.css?$/i,
-                use:[MiniCssExtractPlugin.loader,'css-loader']
+                use:['style-loader','css-loader']
             },
             {
                 test:/\.(ts|js)x?$/i,
@@ -29,19 +30,17 @@ module.exports = {
             {
                 test:/\.(svg|gif)$/i,
                 type:'asset/inline'
+            },
+            {
+                test:/\.(ts|tsx)?$/i,
+                exclude:/node_modules/,
+                use:'ts-loader'
             }
-            // {
-            //     test:/\.(ts|tsx)?$/i,
-            //     exclude:/node_modules/,
-            //     use:'ts-loader'
-            // }
         ]
     },
     resolve:{
         extensions:['.ts','.js','.jsx','.tsx','.json'],
         modules:['node_modules']
     },
-    plugins:[new MiniCssExtractPlugin({
-        filename:'style.css'
-    })]
+    plugins:[]
 }
